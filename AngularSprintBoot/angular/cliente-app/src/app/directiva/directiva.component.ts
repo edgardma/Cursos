f6873e52cda9455012142032ff57fd369da13d41
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-05-04 22:32:12 -0500

    Clase 16: Directiva estructural *ngIf

--- AngularSprintBoot/angular/cliente-app/src/app/directiva/directiva.component.ts
+++ AngularSprintBoot/angular/cliente-app/src/app/directiva/directiva.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent {
 
   listaCurso: string[] = ['TypeScript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+  habilitar: boolean = true;
 
   constructor() { }
 
+  setHabilitar() : void {
+    this.habilitar = !this.habilitar;
+  }
 }
