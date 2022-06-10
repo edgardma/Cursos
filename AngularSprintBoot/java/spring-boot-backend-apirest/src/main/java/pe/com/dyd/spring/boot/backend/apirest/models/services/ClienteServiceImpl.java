cf5a4da81c4b2384227368bcf9462285bc1a90e3
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-06-09 23:59:37 -0500

    Clase 37: Añadiendo las clases Respository (Dao) y Service

---
+++ AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,24 @@
+package pe.com.dyd.spring.boot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pe.com.dyd.spring.boot.backend.apirest.models.dao.IClienteDao;
+import pe.com.dyd.spring.boot.backend.apirest.models.entity.Cliente;
+
+@Service
+public class ClienteServiceImpl implements IClienteService {
+
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public List<Cliente> fidAll() {
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+}
