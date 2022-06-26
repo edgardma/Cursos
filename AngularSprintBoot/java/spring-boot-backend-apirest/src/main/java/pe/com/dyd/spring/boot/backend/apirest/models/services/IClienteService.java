2e6b013a3ab9c698d803c4998abb0b0b0ab19221
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-06-26 02:20:49 -0500

    Clase 44: Escribir los métodos del CRUD en ClienteService

--- AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/services/IClienteService.java
+++ AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/services/IClienteService.java
@@ -7,4 +7,10 @@ import pe.com.dyd.spring.boot.backend.apirest.models.entity.Cliente;
 public interface IClienteService {
 	
 	public List<Cliente> fidAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
 }
