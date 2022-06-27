a38fc249ed6ea7980ae0c7fb79a42053773d1b30
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-06-27 18:33:08 -0500

    Clase 45: Escribiendo los métodos de show y create

--- AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/entity/Cliente.java
+++ AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/entity/Cliente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -21,12 +22,19 @@ public class Cliente implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nombre;
 	private String apellido;
 	private String email;
+	
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	private Date creatAt;
+	
+	@PrePersist
+	public void prePersist() {
+		creatAt = new Date();
+	}
 	
 	public Long getId() {
 		return id;
