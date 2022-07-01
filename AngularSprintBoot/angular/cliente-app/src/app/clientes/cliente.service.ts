7b1beeeaa504c8941d47b6a10c058e3d5a38db2a
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-07-01 17:20:02 -0500

    Clase 51: Escribiendo implementacion crear en el cliente.service.ts y form.component.ts

--- AngularSprintBoot/angular/cliente-app/src/app/clientes/cliente.service.ts
+++ AngularSprintBoot/angular/cliente-app/src/app/clientes/cliente.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
   private urlEndPoint:string = 'http://localhost:8090/api/clientes';
+  private httHeaders = new HttpHeaders({'Content-Type':'application/json'})
+
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    /* Una forma:
-    return this.http.get(this.urlEndPoint).pipe(
-      map( response => response as Cliente[])
-    );
-    */
-    // Otra forma
     return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
+
+  create(cliente: Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httHeaders})
   }
 }
--- AngularSprintBoot/angular/cliente-app/src/app/clientes/form.component.ts
+++ AngularSprintBoot/angular/cliente-app/src/app/clientes/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import {ClienteService} from './cliente.service'
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-form',
@@ -9,14 +11,16 @@ export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente()
   public titulo: string = "Crear Cliente"
 
-  constructor() { }
+  constructor(private clienteService: ClienteService,
+              private router: Router) { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  
   public create(): void {
-    console.log("Clicked!")
     console.log(this.cliente)
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
   }
 
 }
