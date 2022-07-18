65e838d7cb9b39d5cf6cf7463a200589a9b20fa2
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-07-18 09:56:10 -0500

    Clase 56: Escribiendo el Delete en la clase service y el componente clientes

--- AngularSprintBoot/angular/cliente-app/src/app/clientes/clientes.component.ts
+++ AngularSprintBoot/angular/cliente-app/src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-clientes',
@@ -16,6 +17,36 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void{
+    swal({
+      title: '¿Está seguro de eliminar?',
+      text: `¿Seguro que desea eliminar el cliente ${cliente.nombre} ${cliente.apellido}`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar',
+      cancelButtonText: 'No, cancelar',
+      confirmButtonClass: 'btn btm-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: false,
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          responce => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swal(
+              'Cliente Eliminado!',
+              `Usted ha eliminado el cliente  ${cliente.nombre} ${cliente.apellido}`,
+              'success'
+            )
+          }
+        )
+      }
+    })
   }
 
 }
