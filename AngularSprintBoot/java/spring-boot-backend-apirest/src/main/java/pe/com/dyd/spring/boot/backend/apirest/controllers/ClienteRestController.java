5c3bd466664931a5b630058e469a4df1e9adcaa5
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-07-23 19:12:38 -0500

    Clase 62: Manejo de error en el Backend método handler update

--- AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/controllers/ClienteRestController.java
+++ AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/controllers/ClienteRestController.java
@@ -78,15 +78,35 @@ public class ClienteRestController {
 	}
 	
 	@PutMapping("/clientes/{id}")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		
 		Cliente clienteActual = clienteService.findById(id);
+		Cliente clienteUpdate = null;
 		
-		clienteActual.setApellido(cliente.getApellido());
-		clienteActual.setNombre(cliente.getNombre());
-		clienteActual.setEmail(cliente.getEmail());
+		Map<String, Object> response = new HashMap<>();
 		
-		return clienteService.save(clienteActual);
+		if (clienteActual == null) {
+			response.put("mensaje", "Error: no se pudo editar, el cliente ID: ".concat(id.toString().concat(" no existe en la base de datos")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		try {
+			clienteActual.setApellido(cliente.getApellido());
+			clienteActual.setNombre(cliente.getNombre());
+			clienteActual.setEmail(cliente.getEmail());
+			clienteActual.setCreatAt(cliente.getCreatAt());
+			
+			clienteUpdate = clienteService.save(clienteActual);
+		} catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar el UPDATE en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El cliente ha sido actualizado con éxito!");
+		response.put("cliente", clienteUpdate);
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping("/clientes/{id}")
