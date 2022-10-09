684bd77b0cadc515d2911ff6a40bdc978a25cf11
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-10-09 17:55:08 -0500

    Clase 80: Uso del operador tap en el Observable

--- AngularSprintBoot/angular/cliente-app/src/app/clientes/cliente.service.ts
+++ AngularSprintBoot/angular/cliente-app/src/app/clientes/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { formatDate, DatePipe } from '@angular/common'
 import { Cliente } from './cliente';
-import { map, Observable, catchError, throwError } from 'rxjs';
+import { map, Observable, catchError, throwError, tap } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import swal from 'sweetalert2';
 import { Router } from '@angular/router';
@@ -15,16 +15,32 @@ export class ClienteService {
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(this.urlEndPoint).pipe(
+      tap(response => {
+        console.log('ClienteService: tap 1')
+        //let clientes = response as Cliente[]
+        response.forEach( cliente => {
+            console.log(cliente.nombre)
+        }
+        )
+      }),
+
       map(response => {
         return response.map( cliente => {
           cliente.nombre = cliente.nombre.toUpperCase()
           //cliente.creatAt = formatDate(cliente.creatAt, 'dd-MM-yyyy', 'en-US')
-
           //let datePipe = new DatePipe('es-PE')
           //cliente.creatAt = datePipe.transform(cliente.creatAt, 'EEEE dd, MMMM yyyy')
           return cliente
         })
-      })
+      }),
+
+      tap(response => {
+        console.log('ClienteService: tap 2')
+        response.forEach( cliente => {
+            console.log(cliente.nombre)
+        })
+      }),
+
     );
   }
 
