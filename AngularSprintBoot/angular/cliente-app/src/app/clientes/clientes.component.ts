7907b36977f7ffd2e1a2e9f50453823f3d1d4f15
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-10-15 22:23:17 -0500

    Clase 84: Correcion de codigo, poner el atributo content en el response

--- AngularSprintBoot/angular/cliente-app/src/app/clientes/clientes.component.ts
+++ AngularSprintBoot/angular/cliente-app/src/app/clientes/clientes.component.ts
@@ -12,6 +12,7 @@ import { ActivatedRoute } from '@angular/router'
 export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
+  paginador: any;
 
   constructor(private clienteService: ClienteService,
               private activatedRoute: ActivatedRoute) { }
@@ -22,20 +23,20 @@ export class ClientesComponent implements OnInit {
       if(!page) {
         page = 0
       }
-      
+
       this.clienteService.getClientes(page).pipe(
         tap(response => {
           console.log('ClientesComponent: tap 3');
-          (response as Cliente[]).forEach(cliente => {
+          (response.content as Cliente[]).forEach(cliente => {
             console.log(cliente.nombre)
           })
           console.log('ClientesComponent: tap 4');
         })
-      )
-      .subscribe(
-        response => this.clientes = response as Cliente[]
-      );
-    })
+      ).subscribe(response => {
+          this.clientes = response.content as Cliente[]
+          this.paginador = response
+        });
+    });
   }
 
   delete(cliente: Cliente): void{
