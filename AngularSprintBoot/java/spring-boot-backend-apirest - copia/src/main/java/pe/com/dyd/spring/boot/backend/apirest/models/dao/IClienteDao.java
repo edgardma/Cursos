8947f01f30f7c58ab5d445ed5b9ef4caf6e70cbe
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-11-20 21:33:57 -0500

    Depurar repositorio

--- AngularSprintBoot/java/spring-boot-backend-apirest - copia/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/dao/IClienteDao.java
+++
@@ -1,9 +0,0 @@
-package pe.com.dyd.spring.boot.backend.apirest.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import pe.com.dyd.spring.boot.backend.apirest.models.entity.Cliente;
-
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
-	
-}
