1012d6eb699cd6c9b067d75f725bc559e8f9ee75
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-12-24 13:29:20 -0500

    Clase 125: Creando el repositorio JPA IUsuarioDao

---
+++ AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/dao/IUsuarioDao.java
@@ -0,0 +1,15 @@
+package pe.com.dyd.spring.boot.backend.apirest.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import pe.com.dyd.spring.boot.backend.apirest.models.entity.Usuario;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long> {
+	
+	public Usuario findByUsername(String username);
+	
+	@Query("SELECT u FROM usuario u WHERE u.username = ?1")
+	public Usuario findByUsername2(String username);
+	
+}
