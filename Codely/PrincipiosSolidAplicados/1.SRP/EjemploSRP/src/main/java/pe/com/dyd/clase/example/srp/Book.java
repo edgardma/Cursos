3b9d94911e260d60dc82ff9ececa88217c0d08da
Edgard M. Marquez Aylas <edgardma@gmail.com>
2023-01-02 21:39:35 -0500

    Ejemplo SRP: Código que no cumple con el principio de Resonsabilida Única

---
+++ Codely/PrincipiosSolidAplicados/1.SRP/EjemploSRP/src/main/java/pe/com/dyd/clase/example/srp/Book.java
@@ -0,0 +1,15 @@
+package pe.com.dyd.clase.example.srp;
+
+public class Book {
+    public String getTitle() {
+        return "A great book";
+    }
+
+    public String getAuthor() {
+        return "John Doe";
+    }
+
+    public void printCurrentPage() {
+        System.out.println("Current page content");
+    }
+}
