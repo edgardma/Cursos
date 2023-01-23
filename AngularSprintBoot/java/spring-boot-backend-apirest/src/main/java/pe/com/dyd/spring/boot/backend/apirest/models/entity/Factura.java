adf0a1826348cb3756b2e7441337ae1a5a636034
Edgard M. Marquez Aylas <edgardma@gmail.com>
2023-01-22 23:06:19 -0500

    Clase 166: Asociaciones: OneToMany Undireccional - Clases Entity ItemFactura y Producto

--- AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/entity/Factura.java
+++ AngularSprintBoot/java/spring-boot-backend-apirest/src/main/java/pe/com/dyd/spring/boot/backend/apirest/models/entity/Factura.java
@@ -1,8 +1,11 @@
 package pe.com.dyd.spring.boot.backend.apirest.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -26,6 +30,7 @@ public class Factura implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String descripcion;
+	
 	private String observacion;
 	
 	@Column(name="create_at")
@@ -33,9 +38,16 @@ public class Factura implements Serializable {
 	private Date creatAt;
 	
 	@ManyToOne(fetch=FetchType.LAZY)
-	//@JoinColumn(name="cliente_id")
 	private Cliente cliente;
 	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="factura_id")
+	private List<ItemFactura> items;
+	
+	public Factura() {
+		items = new ArrayList<>();
+	}
+
 	@PrePersist
 	public void prePersis() {
 		this.creatAt = new Date();
@@ -70,6 +82,12 @@ public class Factura implements Serializable {
 	}
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
 	}
 	
 }
