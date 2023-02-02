9bc6452eac4f11bcb19a656f7462726ed00c75a8
Edgard M. Marquez Aylas <edgardma@gmail.com>
2023-02-02 08:07:25 -0500

    Clase 174: Creando Componente detalle de la factura y su clase Service relacionada

---
+++ AngularSprintBoot/angular/cliente-app/src/app/facturas/services/facturas.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Factura } from '../models/factura';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FacturasService {
+
+  private urlEndPoint: string = 'http://localhost:8090/api/facturas'
+
+  constructor(private http: HttpClient) { }
+
+  getFactura(id: number): Observable<Factura> {
+    return this.http.get<Factura>(`${this.urlEndPoint}/${id}`)
+  }
+
+}
