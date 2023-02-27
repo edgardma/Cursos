a83b25f1523dc7bb51f6c476eb72a7ee162c84d3
Edgard M. Marquez Aylas <edgardma@gmail.com>
2023-02-27 00:35:35 -0500

    Clase 190: Validando el formulario de la factura

--- AngularSprintBoot/angular/cliente-app/src/app/facturas/facturas.component.ts
+++ AngularSprintBoot/angular/cliente-app/src/app/facturas/facturas.component.ts
@@ -107,10 +107,18 @@ export class FacturasComponent implements OnInit {
     this.factura.items = this.factura.items.filter((item: ItemFactura) => id != item.producto.id)
   }
 
-  create(): void {
-    this.facturaService.create(this.factura).subscribe(factura => {
-      swal(this.titulo, `Factura ${factura.descripcion} creada con éxito!`, 'success')
-      this.router.navigate(['/clientes'])
-    })
+  create(facturaForm): void {
+    console.log(this.factura)
+
+    if (this.factura.items.length == 0) {
+      this.autocompleteControl.setErrors({'invalid': true})
+    }
+
+    if (facturaForm.form.valid && this.factura.items.length > 0) {
+      this.facturaService.create(this.factura).subscribe(factura => {
+        swal(this.titulo, `Factura ${factura.descripcion} creada con éxito!`, 'success')
+        this.router.navigate(['/clientes'])
+      })
+    }
   }
 }
