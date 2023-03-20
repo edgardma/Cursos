059b530a6644f863dbfd72405da9fe0e5966d102
Edgard M. Marquez Aylas <edgardma@gmail.com>
2023-03-20 15:22:25 -0500

    Clase 209: Creando el proyecto backend y configurando el servidor WebSocket (el Broker)

---
+++ AngularSprintBoot/java/spring-boot-backend-chat/src/test/java/pe/com/dyd/springboot/backend/chat/SpringBootBackendChatApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.dyd.springboot.backend.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
