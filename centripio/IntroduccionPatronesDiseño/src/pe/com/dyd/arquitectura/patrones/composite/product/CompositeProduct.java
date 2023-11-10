daf96f254181f46c73eeb6b181803b6b523a032d
Edgard M. Marquez Aylas <edgardma@gmail.com>
2023-11-10 17:54:50 -0500

    Ejemplo de protron Composite

---
+++ centripio/IntroduccionPatronesDiseño/src/pe/com/dyd/arquitectura/patrones/composite/product/CompositeProduct.java
@@ -0,0 +1,50 @@
+package pe.com.dyd.arquitectura.patrones.composite.product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeProduct extends AbstractProduct {
+	
+	private List<AbstractProduct> products;
+	
+	public CompositeProduct(String name, double price) {
+		super(name, price);
+		products = new ArrayList<>();
+	}
+
+	public List<AbstractProduct> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<AbstractProduct> products) {
+		this.products = products;
+	}
+	
+	public void addProduct(AbstractProduct product) {
+		products.add(product);
+	}
+	
+	public void removeProduct(AbstractProduct product) {
+		products.remove(product);
+	}
+	
+	@Override
+	public double getPrice() {
+		double price = 0;
+		for(AbstractProduct current : products) {
+			price += current.getPrice();
+		}
+		
+		return price;
+	}
+
+	@Override
+	public void printOrder() {
+		System.out.println("Package " + this.getName() + " =>");
+		for (AbstractProduct current : products) {
+			current.printOrder();
+		}
+		System.out.println("End Package " + this.getName() + " =>");
+	}
+	
+}
---
+++ centripio/IntroduccionPatronesDiseño/src/pe/com/dyd/arquitectura/patrones/composite/product/SimpleProduct.java
@@ -0,0 +1,20 @@
+package pe.com.dyd.arquitectura.patrones.composite.product;
+
+public class SimpleProduct extends AbstractProduct {
+	
+	private String brand;
+	
+	public SimpleProduct(String name, double price, String brand) {
+		super(name, price);
+		this.brand = brand;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+}
