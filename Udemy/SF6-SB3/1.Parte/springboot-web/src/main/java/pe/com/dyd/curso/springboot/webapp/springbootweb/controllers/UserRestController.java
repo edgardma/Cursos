2ec2d6b3ad7d98ce9ea4fc06721d2fc3facd20a1
Edgard M. Marquez Aylas <edgardma@gmail.com>
2024-11-01 17:24:35 -0500

    Clase 15 - Anotacion RequestMapping

--- Udemy/SF6-SB3/1.Parte/springboot-web/src/main/java/pe/com/dyd/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ Udemy/SF6-SB3/1.Parte/springboot-web/src/main/java/pe/com/dyd/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -3,14 +3,15 @@ package pe.com.dyd.curso.springboot.webapp.springbootweb.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details2")
+    @RequestMapping(path="/details", method = RequestMethod.GET)
     public Map<String, Object> details() {
         Map<String, Object> body = new HashMap<>();
         body.put("title", "Hola Mundo Spring Boot");
