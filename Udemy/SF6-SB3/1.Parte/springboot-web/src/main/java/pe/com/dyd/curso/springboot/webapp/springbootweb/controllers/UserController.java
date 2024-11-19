b68093fcb5f695223fd3649da016e4fd7094b296
Edgard M. Marquez Aylas <edgardma@gmail.com>
2024-11-19 18:00:58 -0500

    Clase 23 - Listas en Thymeleaf

--- Udemy/SF6-SB3/1.Parte/springboot-web/src/main/java/pe/com/dyd/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ Udemy/SF6-SB3/1.Parte/springboot-web/src/main/java/pe/com/dyd/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,7 +1,11 @@
 package pe.com.dyd.curso.springboot.webapp.springbootweb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import pe.com.dyd.curso.springboot.webapp.springbootweb.models.User;
@@ -17,6 +21,15 @@ public class UserController {
         model.addAttribute("user", user);
 
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usuarios!");
+        return "list";
     }
     
 }
