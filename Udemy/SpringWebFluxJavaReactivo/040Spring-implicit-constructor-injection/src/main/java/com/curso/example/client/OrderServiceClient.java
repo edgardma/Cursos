a673685c40013181d13f41156f82d6b43d4d00df
Edgard M. Marquez Aylas <edgardma@gmail.com>
2025-02-25 12:25:41 -0500

    Clase 17

---
+++ Udemy/SpringWebFluxJavaReactivo/040Spring-implicit-constructor-injection/src/main/java/com/curso/example/client/OrderServiceClient.java
@@ -0,0 +1,20 @@
+package com.curso.example.client;
+
+import org.springframework.stereotype.Component;
+
+import com.curso.example.service.OrderService;
+
+@Component
+public class OrderServiceClient {
+
+	private OrderService orderService;
+
+    //@Autowired is no longer required in Spring 4.3 and later.
+    public OrderServiceClient (OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    public void showPendingOrderDetails (String id) {
+        System.out.println(orderService.getOrderDetails(id));
+    }
+}
