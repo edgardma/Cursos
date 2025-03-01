a6297b5a87f9a81052428fa12a826e5c00d4292e
Edgard M. Marquez Aylas <edgardma@gmail.com>
2025-03-01 18:39:00 -0500

    Clase 7 - Creacion de proyecto base

---
+++ Udemy/MasterSpringSecurityJWTOauth2/spring-security-course/src/test/java/pe/com/dyd/cursos/api/springsecuritycourse/SpringSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.dyd.cursos.api.springsecuritycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
