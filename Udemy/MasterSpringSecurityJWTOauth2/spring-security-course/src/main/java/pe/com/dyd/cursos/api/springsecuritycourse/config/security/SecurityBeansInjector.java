a8a50ff473269e069a6b82477ddf838e8ea5e1c3
Edgard M. Marquez Aylas <edgardma@gmail.com>
2025-03-26 19:38:21 -0500

    Clase 20 - Creando nuestro AuthenticationProvider

--- Udemy/MasterSpringSecurityJWTOauth2/spring-security-course/src/main/java/pe/com/dyd/cursos/api/springsecuritycourse/config/security/SecurityBeansInjector.java
+++ Udemy/MasterSpringSecurityJWTOauth2/spring-security-course/src/main/java/pe/com/dyd/cursos/api/springsecuritycourse/config/security/SecurityBeansInjector.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
@@ -17,4 +19,12 @@ public class SecurityBeansInjector {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationStrategy = new DaoAuthenticationProvider();
+        authenticationStrategy.setPasswordEncoder(null);
+        authenticationStrategy.setUserDetailsService(null);
+
+        return authenticationStrategy;
+    }
 }
